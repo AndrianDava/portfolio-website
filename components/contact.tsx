936071456dd0d0caf3beacd36a8b4f01758c9218
Andrian <111829852+AndrianDava@users.noreply.github.com>
2024-04-07 16:05:24 +0000

    Pending changes exported from your codespace

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="andriandavamaulana5@gmail.com">
+          andriandavamaulana5@gmail.com
         </a>{" "}
         or through this form.
       </p>
--- lib/data.ts
+++ lib/data.ts
@@ -35,51 +35,42 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Pendidikan",
+    location: "Tulang Bawang, Unit 2",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+     "Saya bersekolah di SMK Al-iman Tulang Bawang.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+   
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Class",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "Belajar Programing Dari Youtube.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Youtube Dwonloader",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "Saya Membuat Web Ini Karena Request Dari Teman Saya.",
+    tags: ["React", "Next.js",],
     imageUrl: corpcommentImg,
   },
   {
-    title: "rmtDev",
+    title: "Tiktok Dwonloader",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+      "Saya Membuat Web Ini Karena Request Dari Teman Saya.",
+    tags: ["Nodejs",],
     imageUrl: rmtdevImg,
   },
   {
-    title: "Word Analytics",
+    title: "Instagram Downloader",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+      "Saya Membuat Web Ini Karena Request Dari Teman Saya.",
+    tags: ["React", "Next.js", "Node js",],
     imageUrl: wordanalyticsImg,
   },
 ] as const;
@@ -88,20 +79,8 @@ export const skillsData = [
   "HTML",
   "CSS",
   "JavaScript",
-  "TypeScript",
-  "React",
   "Next.js",
   "Node.js",
-  "Git",
-  "Tailwind",
-  "Prisma",
   "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
   "Python",
-  "Django",
-  "Framer Motion",
 ] as const;
